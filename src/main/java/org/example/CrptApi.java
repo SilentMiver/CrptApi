930fd5c155c4862088678c12c6fc6a44f6884655
SexyBoy12 <russianthief3@gmail.com>
2024-03-02 13:55:39 +0300

    change HttpClient to OkHttpClient Change AtomicInteger to Semaphore add jackson's ObjectMapper add gradle dependencies

--- src/main/java/org/example/CrptApi.java
+++ src/main/java/org/example/CrptApi.java
@@ -1,19 +1,26 @@
 package org.example;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.squareup.okhttp.OkHttpClient;
+
 import java.net.http.HttpClient;
 import java.time.Instant;
+import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class CrptApi {
     public static final String API_LINK = "https://ismp.crpt.ru/api/v3/lk/documents/create";
-    private final HttpClient httpClient;
-    private final AtomicInteger requestCounter;
+    private final OkHttpClient httpClient;
+    private final Semaphore semaphore;
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     public CrptApi(TimeUnit timeUnit, int request_limit) {
-        this.httpClient = HttpClient.newHttpClient();
-        this.requestCounter = new AtomicInteger(request_limit);
+        this.semaphore = new Semaphore(request_limit);
+        this.httpClient = new OkHttpClient();
+
 
     }
+
 
 }
