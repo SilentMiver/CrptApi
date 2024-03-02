475bd7ca25539ca70319acb7c258d6e3294bf57f
SexyBoy12 <russianthief3@gmail.com>
2024-03-02 13:39:18 +0300

    add HttpClient and AtomicInteger in CrptApi

--- src/main/java/org/example/CrptApi.java
+++ src/main/java/org/example/CrptApi.java
@@ -1,4 +1,19 @@
 package org.example;
 
+import java.net.http.HttpClient;
+import java.time.Instant;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class CrptApi {
+    public static final String API_LINK = "https://ismp.crpt.ru/api/v3/lk/documents/create";
+    private final HttpClient httpClient;
+    private final AtomicInteger requestCounter;
+
+    public CrptApi(TimeUnit timeUnit, int request_limit) {
+        this.httpClient = HttpClient.newHttpClient();
+        this.requestCounter = new AtomicInteger(request_limit);
+
+    }
+
 }
